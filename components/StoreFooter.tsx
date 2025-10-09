f9248f3077fe5b23fe39a5f75452602624d9c24a
Cedrick A.k.a ErolledDev <107593932+ErolledDev@users.noreply.github.com>
2025-10-09 21:58:38 +0800

    Update StoreFooter.tsx

--- components/StoreFooter.tsx
+++ components/StoreFooter.tsx
@@ -7,10 +7,10 @@ export default function StoreFooter() {
   return (
     <footer className="bg-white mt-0 pt-12 pb-12 px-4 sm:px-6">
       <div className="flex justify-center items-center">
-        <p className="text-xs text-gray-500 flex items-center">
+        <p className="text-2xl text-gray-500 flex items-center">
           Powered by{' '}
           <span className="font-semibold text-gray-700 flex items-center ml-1">
-            <Store className="w-3 h-3 mr-1" />
+            <Store className="w-8 h-8 mr-1 text-emerald-600" />
             Tiangge
           </span>
         </p>
