c6586efcc61ec1ebe803a461f76db7c61d30a3c5
Cedrick A.k.a ErolledDev <107593932+ErolledDev@users.noreply.github.com>
2025-10-28 22:58:27 +0800

    init

--- components/StoreFooter.tsx
+++ components/StoreFooter.tsx
@@ -4,11 +4,12 @@ import React from 'react';
 
 export default function StoreFooter() {
   return (
-    <footer className="bg-white py-4 text-center text-gray-600">
+    // Updated vertical padding to 'py-8' for more space on top and bottom
+    <footer className="bg-white py-8 text-center text-gray-600">
       <div className="space-y-3">
         {/* Tiangge Heading */}
-        <div className="py-8">
-          <p className="text-xl font-bold text-gray-400">
+        <div>
+          <p className="text-xl font-bold text-gray-700">
             Tiangge
           </p>
         </div>
